043b5b29994e4565ad4050b37c5c05367d210708
tisgitau <gitausamson67@gmail.com>
2022-05-27 08:33:54 +0300

    lesson14.py

--- lesson13.py
+++ lesson13.py
@@ -7,18 +7,32 @@
 #{"key":"value"}
 # for python we use {}
 name = 'John Cena'
-colous = {'colour':'red''green''blue'}
+colours = {'colour':'red''green''blue'}
 
 cars = {'type':'toyota','plate_number':'fsgteiek'}
 #print ( cars['type'] )
 #print (cars)['plate_number']
 #print (cars)['plate_number'],(cars)['type']
 person = {'name':'Sam','gender':'male','residence':'Kiambu'}
-person = {'age':'21'}
-person = {'colour':'red'}
+person  ["age"] = {'21'}
+person  ["colour"]={'red'}
 #print (person)['name'],person['gender']
 #print (person)
 #print (person[3]del())
 #for looping  over dictionaries
-for key,value in person.items():
-    print(f"{key}:{value}")
+#f
+#for key,value in person.items():
+    #print(f"{key}:{value}")
+#to access values in a dictionary
+#print(person['gender'])
+#print(person['gender']) 
+#print(person.get ("password','the'residence'key is non existant"))  
+mary_fav_food=['beef','chips','vegetables']
+jane_fav_food=['rice','ugali','potatoes']
+fav_food ={
+    'Mary':'beef''chips''vegetables',
+    'Jane':'rice'}
+#print(fav_food)
+person = {'name':'Jane','email':'jane@67','password':'12345'
+,             'name':'Mary','email':'mary@67','password':'9876'}
+print(person)
---
+++ lesson14.py
@@ -0,0 +1,35 @@
+#1/user/bin/python
+#name : Sam Gitau
+#date : 25/5/2022
+
+#print(range(0,20))
+#for number in range (0,20):
+    #if (number%2  == 0):
+        #print(number)
+
+#sum = 0       
+#for number in range(0,50):
+    #if(number%2 == 0):
+       #sum = sum +number 
+#print(sum)
+#product = 1
+#for number in range(0,20):
+    #if(number%2 == 1):
+        #product = product * number
+#print(product)
+
+#factorial of 6 (multiplying number with ) 
+#< less than
+# >greater than
+# >= greater or equal
+# = is an assigment
+# == equal 
+# * multiply
+# / division
+number = 10
+while  number <10:
+    if(number %2== 0):
+        print(number)
+    number = number + 1
+
+
